ca65388feb8294f8c3951993b51ebc03939a9b41
GalicIvan <ivangalic2004@gmail.com>
2024-03-06 18:53:59 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = []
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+document.getElementById('popupBtn').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+  });
+  
+  document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+  });
+  
